2d9e0f63e6ae833f678bda7afd2a96117189e1a6
Micah-Lander <lander.22@daytonstemschool.org>
2024-12-19 16:05:58 -0500

    This is a test commit.

---
+++ src/main/java/frc/robot/TankDrive.java
@@ -0,0 +1,21 @@
+package frc.robot;
+// add imports
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.Encoder;
+
+public class TankDrive {
+//only this file can use the variable
+//value never changes
+// The zero means the port number
+private final DCMotor LeftDriveMotor = 0;
+private final Encoder LeftDriveEncoder = 0;
+private final DCMotor RightDriveMotor = 0;
+private final Encoder RightDriveEncoder = 0;
+// Each joystick controls a different side of the robot and awe have to set them
+// 3 meters per second
+public static final int maxSpeed = 3;
+
+
+
+}
